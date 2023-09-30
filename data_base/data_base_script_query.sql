1dbef0b5ee15e16db9366cc38cf1d4bf6ef8f193
boniich <ezequieldbo25@gmail.com>
2023-09-30 14:22:27 -0300

    feat: add new table and other sql lines

--- data_base/data_base_script_query.sql
+++ data_base/data_base_script_query.sql
@@ -1,5 +1,8 @@
 use ecommerce_igniter;
 
+
+###  Adminis
+
 /*
 CREATE TABLE admins (
         id int NOT NULL AUTO_INCREMENT,
@@ -13,4 +16,21 @@ CREATE TABLE admins (
 insert into admins (full_name,email,password,image) values ('admin', 'admin@gmail.com','123456',null);
 */
 
+### Products
+/*
+CREATE TABLE products (
+        id int NOT NULL AUTO_INCREMENT,
+        name varchar(70) not null,
+        description varchar(150) NULL ,
+        image varchar(100) null,
+        stock int not null,
+        price double not null,
+        PRIMARY KEY (id)
+);
+
+insert into products (name,description,image,stock,price) values ('Teclado Noga', 'Este es un teclado noga',null, 10, 50.5);
+insert into products (name,description,image,stock,price) values ('Mouse Noga', 'Este es un mouse noga',null, 10, 41.5);
+*/
+
 select * from admins;
+select * from products;
