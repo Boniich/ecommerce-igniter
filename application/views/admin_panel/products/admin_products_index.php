09e0608b83ebfa1dc42bfe1e25836aaaa599d141
boniich <ezequieldbo25@gmail.com>
2023-10-01 15:20:46 -0300

    feat: add button to create a product

--- application/views/admin_panel/products/admin_products_index.php
+++ application/views/admin_panel/products/admin_products_index.php
@@ -8,6 +8,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <div class="container">
         <div class="my-5">
             <h1 class="text-center">Admin panel - products</h1>
+            <td><a href="#" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createProductModal">
+                    Create Product
+                </a></td>
         </div>
 </body>
 
