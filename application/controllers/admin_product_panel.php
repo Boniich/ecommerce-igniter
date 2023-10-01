26d9e3557082d55168b552170afa5093cd31b15c
boniich <ezequieldbo25@gmail.com>
2023-10-01 15:21:10 -0300

    feat: add the load of modal to create a product

--- application/controllers/admin_product_panel.php
+++ application/controllers/admin_product_panel.php
@@ -3,9 +3,13 @@
 class Admin_product_panel extends CI_Controller
 {
 
+    private string $_path_view_folder = 'admin_panel/products/';
+
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('url_helper');
         $this->load->model('admin_product_model');
     }
 
@@ -16,6 +20,7 @@ class Admin_product_panel extends CI_Controller
         $this->load->view('head/head', $data);
         $this->load->view('admin_panel/products/admin_products_index');
         $this->load->view('admin_panel/products/show_products_table');
+        $this->load->view($this->_path_view_folder . '/modals/create_product_modal');
         $this->load->view('admin_panel/products/modals/delete_product_modal');
     }
 
