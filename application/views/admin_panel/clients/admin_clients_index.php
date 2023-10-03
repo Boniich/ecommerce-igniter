55025d3b5cb9479d624e9613b99ba2a5e7f00899
boniich <ezequieldbo25@gmail.com>
2023-10-03 15:00:42 -0300

    feat: add button to open the modal to create a new product

--- application/views/admin_panel/clients/admin_clients_index.php
+++ application/views/admin_panel/clients/admin_clients_index.php
@@ -8,6 +8,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <div class="container">
         <div class="my-5">
             <h1 class="text-center">Admin panel - Clients</h1>
+            <a href="#" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createClientModal">
+                Create Product
+            </a>
         </div>
 </body>
 
