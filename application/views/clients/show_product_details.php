fb959e2cc5332f8a68cb314024bd627e1c07fefb
boniich <ezequieldbo25@gmail.com>
2023-10-10 15:15:33 -0300

    feat: add view to show details of products

---
+++ application/views/clients/show_product_details.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<div class="container">
+    <div class="row">
+        <?php foreach ($product as $item) : ?>
+            <div class="col-sm-4">
+                <?php if ($item['image'] == NULL || $item['image'] == '') {
+                    $image = './assets/not-image.png';
+                } else {
+                    $image = './uploads/' . $item['image'];
+                } ?>
+                <img src="<?php echo base_url($image); ?>" class="card-img-top" alt="...">
+            </div>
+            <div class="col-sm">
+                <p>Marca</p>
+                <h3><?php echo $item['name'] ?></h3>
+                <p><?php echo $item['description'] ?></p>
+                <p>Cantidad disponible: <?php echo $item['stock'] ?></p>
+                <p>$<?php echo $item['price'] ?></p>
+                <button>Agregar al carrito</button>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+</div>
+</body>
+
+</html>
