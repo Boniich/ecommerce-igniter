e93080a3dbfc93c4173e0f2a5fb92aad5b5384e8
boniich <ezequieldbo25@gmail.com>
2023-10-10 21:58:24 -0300

    feat: add new nav for when a user is not authenticated

---
+++ application/views/navs/auth_nav/auth_nav.php
@@ -0,0 +1,13 @@
+<header>
+    <ul class="nav p-2 bg-dark nav-expand-lg justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Login clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Register Clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn btn-warning" href="#">Login Admin</a>
+        </li>
+    </ul>
+</header>
