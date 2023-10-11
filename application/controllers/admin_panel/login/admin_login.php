1400c824573c5ebeae1d4b82b55a9657f05c250c
boniich <ezequieldbo25@gmail.com>
2023-10-10 21:59:57 -0300

    feat: add a folder for controller about admin

---
+++ application/controllers/admin_panel/login/admin_login.php
@@ -0,0 +1,36 @@
+<?php
+class Admin_login extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('url_helper');
+        $this->load->model('admin_panel/auth/admin_login_model');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Admin Login';
+        $this->load->view('head/head', $data);
+        $this->load->view('navs/auth_nav/auth_nav');
+        $this->load->view('auth/admin_auth/admin_login');
+    }
+
+    public function do_login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        if ($this->admin_login_model->login($email, $password)) {
+            redirect('admin_panel/products');
+        } else {
+            $data['error_message'] = 'Invalid username or password';
+            $data['title'] = 'Admin Login';
+            $this->load->view('head/head', $data);
+            $this->load->view('navs/auth_nav/auth_nav');
+            $this->load->view('auth/admin_auth/admin_login', $data);
+        }
+    }
+}
