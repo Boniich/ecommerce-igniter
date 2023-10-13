f2de18629e0a93245dddc4b23fde65548efed5c9
boniich <ezequieldbo25@gmail.com>
2023-10-13 09:31:17 -0300

    feat: make possible authorizate a client

--- application/controllers/clients/auth/client_login.php
+++ application/controllers/clients/auth/client_login.php
@@ -31,6 +31,7 @@ class Client_login extends CI_Controller
         $password = $this->input->post('password');
 
         if ($this->client_login_model->login($email, $password)) {
+            $this->_set_auth_data();
             redirect('products');
         } else {
             $data['error_message'] = 'Invalid username or password';
@@ -39,5 +40,12 @@ class Client_login extends CI_Controller
             $this->load->view('navs/auth_nav/auth_nav');
             $this->load->view('auth/client_auth/client_login');
         }
+    }
+
+    private function _set_auth_data()
+    {
+        $id = $this->client_login_model->get_client_id();
+        $auth_data = array('id' => $id, 'login_in' => true, 'role' => 'client');
+        $this->session->set_userdata($auth_data);
     }
 }
