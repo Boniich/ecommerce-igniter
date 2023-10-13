ce16144d768be1c5ba5f2b577c83d58246d3a1f4
boniich <ezequieldbo25@gmail.com>
2023-10-13 09:40:03 -0300

    refactor: change the name of file to give a better name

---
+++ application/views/navs/unauthenticated_nav/unauthenticated_nav.php
@@ -0,0 +1,13 @@
+<header>
+    <ul class="nav p-2 bg-dark nav-expand-lg justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="<?php echo base_url('client_login') ?>">Login clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url('client_register') ?>">Register Clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn btn-warning" href="<?php echo base_url('admin_login') ?>">Login Admin</a>
+        </li>
+    </ul>
+</header>
