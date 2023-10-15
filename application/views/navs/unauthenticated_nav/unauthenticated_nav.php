d1ac48ec15609d3a59a2ad1758721691d1a0afe4
boniich <ezequieldbo25@gmail.com>
2023-10-15 12:09:31 -0300

    feat: improve unauthenticated nav

--- application/views/navs/unauthenticated_nav/unauthenticated_nav.php
+++ application/views/navs/unauthenticated_nav/unauthenticated_nav.php
@@ -1,13 +1,26 @@
 <header>
-    <ul class="nav p-2 bg-dark nav-expand-lg justify-content-end">
-        <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="<?php echo base_url('client_login') ?>">Login clientes</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url('client_register') ?>">Register Clientes</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link btn btn-warning" href="<?php echo base_url('admin_login') ?>">Login Admin</a>
-        </li>
-    </ul>
+    <nav class="navbar-dark bg-dark p-2">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <p class="m-0 text-light">Ecommerce Igniter 🔥</p>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="<?php echo base_url('products'); ?>">Products</a>
+                </li>
+            </ul>
+            <div>
+                <ul class="nav bg-dark nav-expand-lg justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="<?php echo base_url('client_login') ?>">Login clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url('client_register') ?>">Register Clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-warning" href="<?php echo base_url('admin_login') ?>">Login Admin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </div>
+    </nav>
 </header>
