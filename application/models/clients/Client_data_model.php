f4aaf069cb7451a314d1bdec88cd2aadfeeec1e6
boniich <ezequieldbo25@gmail.com>
2023-10-15 10:22:42 -0300

    feat: add model to bring client data

---
+++ application/models/clients/Client_data_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Client_data_model extends CI_Model
+{
+    private string $_table = 'clients';
+
+    public function get_client($id)
+    {
+        $this->db->select('full_name, image')->where('id', $id);
+        $query = $this->db->get($this->_table);
+
+        $result = $query->result_array();
+        return $result;
+    }
+}
