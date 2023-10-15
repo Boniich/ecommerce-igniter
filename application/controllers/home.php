007f24ecdeff1817c4de4020c12a82ac2ae1694b
boniich <ezequieldbo25@gmail.com>
2023-10-15 17:00:01 -0300

    feat: render 5 productos in home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,7 +8,7 @@ class Home extends CI_Controller
         parent::__construct();
         $this->load->helper('form');
         $this->load->helper('url_helper');
-        $this->load->model('clients/client_product_model');
+        $this->load->model('home/home_product_model');
         $this->load->model('clients/client_data_model');
         $this->load->model('admin_panel/admin_data_model');
         $this->load->library('session');
@@ -17,7 +17,7 @@ class Home extends CI_Controller
     public function index()
     {
         $data['title'] = 'Products';
-        // $data['products'] = $this->client_product_model->get_all_products();
+        $data['products'] = $this->home_product_model->get_products_home();
         $this->load->view('head/head', $data);
         $this->_get_nav();
         $this->load->view('home/home');
