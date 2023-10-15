f5c37e11ac97a6c504c50f71d384d84e4188e89b
boniich <ezequieldbo25@gmail.com>
2023-10-15 19:56:01 -0300

    refactor: improve view of product details

--- application/views/clients/show_product_details.php
+++ application/views/clients/show_product_details.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 <div class="container">
-    <div class="row">
+    <div class="row mt-5">
         <?php foreach ($product as $item) : ?>
             <div class="col-sm-4">
                 <?php if ($item['image'] == NULL || $item['image'] == '') {
@@ -13,17 +13,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 <img src="<?php echo base_url($image); ?>" class="card-img-top" alt="...">
             </div>
             <div class="col-sm">
-                <p>Marca</p>
+                <!-- <p>Marca</p> -->
                 <h3><?php echo $item['name'] ?></h3>
                 <p><?php echo $item['description'] ?></p>
                 <p>Cantidad disponible: <?php echo $item['stock'] ?></p>
-                <p>$<?php echo $item['price'] ?></p>
+                <p class="text-dark fs-3"><b>$<?php echo $item['price'] ?></b></p>
                 <?php if (!$this->session->login_in) : ?>
-                    <p>Debes Iniciar session para comprar</p>
+                    <div class="alert alert-info" role="alert">
+                        Debes Iniciar session para comprar
+                    </div>
                 <?php elseif ($this->session->login_in && $this->session->role === 'client') : ?>
-                    <button>Agregar al carrito</button>
+                    <button class="btn btn-primary">Agregar al carrito</button>
                 <?php elseif ($this->session->login_in && $this->session->role === 'admin') : ?>
-                    <p>Lo sentimos. Los administradores NO PUEDEN COMPRAR</p>
+                    <div class="alert alert-warning" role="alert">
+                        Lo sentimos. Los administradores <b>NO PUEDEN COMPRAR</b>
+                    </div>
                 <?php endif ?>
             </div>
         <?php endforeach; ?>
