cdaab476d7e42c2c8ae3396f01351e92213cc8ca
boniich <ezequieldbo25@gmail.com>
2023-10-20 10:14:06 -0300

    feat: change color links of navs

--- application/views/navs/unauthenticated_nav/unauthenticated_nav.php
+++ application/views/navs/unauthenticated_nav/unauthenticated_nav.php
@@ -4,19 +4,19 @@
             <p class="m-0 text-light">Ecommerce Igniter 🔥</p>
             <ul class="nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="<?php echo base_url('/'); ?>">Home</a>
+                    <a class="nav-link text-white" aria-current="page" href="<?php echo base_url('/'); ?>">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="<?php echo base_url('products'); ?>">Productos</a>
+                    <a class="nav-link text-white" aria-current="page" href="<?php echo base_url('products'); ?>">Productos</a>
                 </li>
             </ul>
             <div>
                 <ul class="nav bg-dark nav-expand-lg justify-content-end">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="<?php echo base_url('client_login') ?>">Ingresar Cliente</a>
+                        <a class="nav-link text-white" aria-current="page" href="<?php echo base_url('client_login') ?>">Ingresar Cliente</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo base_url('client_register') ?>">Registrar Cliente</a>
+                        <a class="nav-link text-white" href="<?php echo base_url('client_register') ?>">Registrar Cliente</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link btn btn-warning" href="<?php echo base_url('admin_login') ?>">Ingresar Admin</a>
