2f0ae0cd7e6ca6ddb80c0f72576102b8b3000759
boniich <ezequieldbo25@gmail.com>
2023-11-02 13:30:58 -0300

    feat: add controller to shopping car and show an initial view of it

---
+++ application/controllers/clients/shopping_car/shopping_car.php
@@ -0,0 +1,37 @@
+<?php
+
+class Shopping_car extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url_helper');
+        $this->load->model('clients/client_data_model');
+        $this->load->library('session');
+    }
+
+
+    public function index()
+    {
+        $data['title'] = 'Carrito de compras';
+        $this->load->view('head/head', $data);
+        $this->get_nav();
+        $this->load->view('clients/shopping_car/shopping_car_index');
+    }
+
+    private function _get_client_data()
+    {
+        $id = $this->session->id;
+        $data = $this->client_data_model->get_client($id);
+        return $data;
+    }
+
+    function get_nav()
+    {
+        if ($this->session->role === "client") {
+            $data['client'] = $this->_get_client_data();
+            $this->load->view('navs/client_nav/client_nav', $data);
+        }
+    }
+}
