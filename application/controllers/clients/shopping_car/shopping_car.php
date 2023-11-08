ec3dae48d11b52fa4b26f360d27e200b5ba83c11
boniich <ezequieldbo25@gmail.com>
2023-11-08 12:06:49 -0300

    refactor: improve code that show alert at add one product at shopping car

--- application/controllers/clients/shopping_car/shopping_car.php
+++ application/controllers/clients/shopping_car/shopping_car.php
@@ -45,24 +45,16 @@ class Shopping_car extends CI_Controller
 
     private function increce_amount_of_duplicate_product_in_car($data)
     {
-        if ($this->shopping_car_model->increce_amount_of_duplicate_product_in_car($data)) {
-            $this->session->set_flashdata('sucessfully_alert', 'El Producto esta duplicado');
-            redirect('product/' . $data['product_id']);
-        } else {
-            $this->session->set_flashdata('error_alert', 'Ups! Algo salio mal! No pudimos agregar tu producto');
-            redirect('product/' . $data['product_id']);
-        }
+        $product_id = $data['product_id'];
+        $result = $this->shopping_car_model->increce_amount_of_duplicate_product_in_car($data);
+        $this->dispach_alert($result, $product_id);
     }
 
     private function add_new_product_to_car($data)
     {
-        if ($this->shopping_car_model->add_product_to_car($data)) {
-            $this->session->set_flashdata('sucessfully_alert', 'Producto agregado al carrito con exito');
-            redirect('product/' . $data['product_id']);
-        } else {
-            $this->session->set_flashdata('error_alert', 'Ups! Algo salio mal! No pudimos agregar tu producto');
-            redirect('product/' . $data['product_id']);
-        }
+        $product_id = $data['product_id'];
+        $result = $this->shopping_car_model->add_product_to_car($data);
+        $this->dispach_alert($result, $product_id);
     }
 
     private function check_duplicate_product_in_car($client_id, $product_id)
@@ -72,6 +64,27 @@ class Shopping_car extends CI_Controller
         } else {
             return false;
         }
+    }
+
+    private function dispach_alert($result, $product_id)
+    {
+        if ($result) {
+            $this->show_successfully_alert($product_id);
+        } else {
+            $this->show_error_alert($product_id);
+        }
+    }
+
+    private function show_successfully_alert($product_id)
+    {
+        $this->session->set_flashdata('sucessfully_alert', 'Producto agregado al carrito con exito');
+        redirect('product/' . $product_id);
+    }
+
+    private function show_error_alert($product_id)
+    {
+        $this->session->set_flashdata('error_alert', 'Ups! Algo salio mal! No pudimos agregar tu producto');
+        redirect('product/' . $product_id);
     }
 
     private function _get_client_data()
