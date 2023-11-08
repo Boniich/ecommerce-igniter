bb4dadf1f10f8cf863d6d0e95f7da2782ebe914a
boniich <ezequieldbo25@gmail.com>
2023-11-07 21:35:32 -0300

    feat: add a model to handle the shopping car of a client

---
+++ application/models/clients/shopping_car/Shopping_car_model.php
@@ -0,0 +1,20 @@
+<?php
+
+class Shopping_car_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function get_product_in_shopping_car($client_id)
+    {
+        $query = $this->db->select('p.id,p.name,p.price,s.product_amount')
+            ->from('products p')->join('shopping_car s', 's.product_id = p.id')
+            ->where('client_id', $client_id)->get();
+
+        $result = $query->result_array();
+        return $result;
+    }
+}
