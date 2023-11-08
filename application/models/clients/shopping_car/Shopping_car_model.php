201e64276f40dda9c119e8f88297e5500482828b
boniich <ezequieldbo25@gmail.com>
2023-11-08 11:38:35 -0300

    feat: add functions to check a duplicate product and increce amount if it is already added to car

--- application/models/clients/shopping_car/Shopping_car_model.php
+++ application/models/clients/shopping_car/Shopping_car_model.php
@@ -18,6 +18,35 @@ class Shopping_car_model extends CI_Model
         return $result;
     }
 
+    public function check_duplicate_product_in_car($client_id, $product_id)
+    {
+        $query = $this->db->where('client_id', $client_id)->where('product_id', $product_id)->get('shopping_car');
+        if ($query->num_rows() >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function increce_amount_of_duplicate_product_in_car($data)
+    {
+        $client_id = $data['client_id'];
+        $product_id = $data['product_id'];
+        $amount = $data['product_amount'];
+
+        $query = $this->db->where('client_id', $client_id)->where('product_id', $product_id)->select('product_amount')->get('shopping_car');
+        $row = $query->row();
+        $actual_amount = $row->product_amount + $amount;
+        $newData = array('client_id' => $client_id, 'product_id' => $product_id, 'product_amount' => $actual_amount);
+
+        $is_updated = $this->db->where('client_id', $client_id)->where('product_id', $product_id)->update('shopping_car', $newData);
+        if ($is_updated) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function add_product_to_car($data)
     {
         if ($this->db->insert('shopping_car', $data)) {
