dcbcfad911f864916a49023debb36ca8355f101f
boniich <ezequieldbo25@gmail.com>
2023-11-08 11:37:43 -0300

    feat: make that if product is already added to cart of a client it will increse the request amount

--- application/controllers/clients/shopping_car/shopping_car.php
+++ application/controllers/clients/shopping_car/shopping_car.php
@@ -36,12 +36,41 @@ class Shopping_car extends CI_Controller
         $amount = $this->input->post('amount');
         $data = array('client_id' => $client_id, 'product_id' => $product_id, 'product_amount' => $amount);
 
-        if ($this->shopping_car_model->add_product_to_car($data)) {
-            $this->session->set_flashdata('sucessfully_alert', 'Producto agregado al carrito con exito');
-            redirect('product/' . $product_id);
+        if ($this->check_duplicate_product_in_car($client_id, $product_id)) {
+            $this->increce_amount_of_duplicate_product_in_car($data);
+        } else {
+            $this->add_new_product_to_car($data);
+        }
+    }
+
+    private function increce_amount_of_duplicate_product_in_car($data)
+    {
+        if ($this->shopping_car_model->increce_amount_of_duplicate_product_in_car($data)) {
+            $this->session->set_flashdata('sucessfully_alert', 'El Producto esta duplicado');
+            redirect('product/' . $data['product_id']);
         } else {
             $this->session->set_flashdata('error_alert', 'Ups! Algo salio mal! No pudimos agregar tu producto');
-            redirect('product/' . $product_id);
+            redirect('product/' . $data['product_id']);
+        }
+    }
+
+    private function add_new_product_to_car($data)
+    {
+        if ($this->shopping_car_model->add_product_to_car($data)) {
+            $this->session->set_flashdata('sucessfully_alert', 'Producto agregado al carrito con exito');
+            redirect('product/' . $data['product_id']);
+        } else {
+            $this->session->set_flashdata('error_alert', 'Ups! Algo salio mal! No pudimos agregar tu producto');
+            redirect('product/' . $data['product_id']);
+        }
+    }
+
+    private function check_duplicate_product_in_car($client_id, $product_id)
+    {
+        if ($this->shopping_car_model->check_duplicate_product_in_car($client_id, $product_id)) {
+            return true;
+        } else {
+            return false;
         }
     }
 
