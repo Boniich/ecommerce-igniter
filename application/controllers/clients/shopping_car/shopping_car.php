8ffadc1b3f2b4a0b339700af105f7159d092799d
boniich <ezequieldbo25@gmail.com>
2023-11-09 14:51:10 -0300

    feat: add logic about decide which view show if there is product or not into a function

--- application/controllers/clients/shopping_car/shopping_car.php
+++ application/controllers/clients/shopping_car/shopping_car.php
@@ -23,10 +23,14 @@ class Shopping_car extends CI_Controller
     {
         $data['title'] = 'Carrito de compras';
         $is_there_products = $this->shopping_car_model->is_there_products_in_shopping_car($this->session->id);
-
         $this->load->view('head/head', $data);
         $this->load->view('navs/modals/exit_modal');
         $this->get_nav();
+        $this->show_shopping_car_view($is_there_products);
+    }
+
+    private function show_shopping_car_view($is_there_products)
+    {
         $this->load->view('clients/shopping_car/shopping_car_index');
         if ($is_there_products) {
             $data['products_in_car'] = $this->shopping_car_model->get_product_in_shopping_car($this->session->id);
