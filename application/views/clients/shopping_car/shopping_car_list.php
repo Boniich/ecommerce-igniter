303da2319f50f16da89aa545983ee7a4827b64e2
boniich <ezequieldbo25@gmail.com>
2023-11-17 17:57:37 -0300

    feat: fix wrong link about products details in shopping car

--- application/views/clients/shopping_car/shopping_car_list.php
+++ application/views/clients/shopping_car/shopping_car_list.php
@@ -11,7 +11,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <th scope="col">Cantidad solicitada</th>
             <th scope="col">Precio unitario</th>
             <th scope="col">P.Total x Producto</th>
-            <th scope="col">Detalles</th>
             <th scope="col">Editar</th>
             <th scope="col">Eliminar</th>
         </tr>
@@ -21,12 +20,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <tr>
                 <td><?php echo $key + 1 ?></td>
                 <th scope="row"><?php echo $product['id']; ?></th>
-                <td><?php echo $product['name']; ?> </td>
+                <td><a class="link-dark fw-bolder text-decoration-none" href="<?php echo base_url('product/') . $product['id']; ?>"><?php echo $product['name']; ?> </a></td>
                 <td class="card-text"><?php echo $product['product_amount']; ?></td>
                 <td>$<?php echo $product['price']; ?></td>
                 <td>$<?php echo $product['total_x_product']; ?></td>
-
-                <td><a href="<?php echo base_url('admin_panel/product/') . $product['id']; ?>">Detalles</a></td>
                 <td><a href="#" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editProductFromCarModal" data-product-id="<?php echo $product['id']; ?>" data-product-stock="<?php echo $product['stock']; ?>" data-product-amount="<?php echo $product['product_amount']; ?>">
                         Editar
                     </a></td>
