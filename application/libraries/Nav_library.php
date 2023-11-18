d05d64894808869128d7c033c06efb3aa6f41959
boniich <ezequieldbo25@gmail.com>
2023-11-18 08:55:49 -0300

    feat: make possible show the client nav when is required only this type of nav

--- application/libraries/Nav_library.php
+++ application/libraries/Nav_library.php
@@ -10,10 +10,26 @@ class Nav_library
         $this->CI = &get_instance();
         $this->CI->load->model('clients/client_data_model');
         $this->CI->load->model('admin_panel/admin_data_model');
+        $this->CI->load->library('session');
     }
 
     public function unauthenticated_nav()
     {
         $this->CI->load->view('navs/unauthenticated_nav/unauthenticated_nav');
+    }
+
+    public function load_client_nav()
+    {
+        if ($this->CI->session->role === "client") {
+            $data['client'] = $this->_get_client_data();
+            $this->CI->load->view('navs/client_nav/client_nav', $data);
+        }
+    }
+
+    private function _get_client_data()
+    {
+        $id = $this->CI->session->id;
+        $data = $this->CI->client_data_model->get_client($id);
+        return $data;
     }
 }
