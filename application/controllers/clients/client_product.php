964124ff635c4ac2ea4f533bbe481e68f2d1fdee
boniich <ezequieldbo25@gmail.com>
2023-12-06 15:11:51 -0300

    feat: Implement pagination library to product's list

--- application/controllers/clients/client_product.php
+++ application/controllers/clients/client_product.php
@@ -2,6 +2,10 @@
 
 class Client_product extends CI_Controller
 {
+    private string $base_url;
+    private int $per_page;
+    private int $page;
+    private int $count_products = 0;
 
     public function __construct()
     {
@@ -10,12 +14,19 @@ class Client_product extends CI_Controller
         $this->load->helper('url_helper');
         $this->load->model('clients/client_product_model');
         $this->load->library('nav_library');
+        $this->load->library('pagination/pagination_library');
+
+        $this->base_url = 'http://localhost/ecommerceIgniter/clients/client_product/index/';
+        $this->pagination_library->set_base_url($this->base_url);
     }
 
     public function index()
     {
+        $this->_initiate_pagination();
+
         $data['title'] = 'Productos';
-        $data['products'] = $this->client_product_model->get_all_products();
+        $data['products'] = $this->client_product_model->get_products($this->per_page, $this->page);
+        $data['links'] = $this->pagination_library->get_links();
         $this->load->view('head/head', $data);
         $this->nav_library->load_common_nav();
         $this->load->view('navs/modals/exit_modal');
@@ -33,5 +44,15 @@ class Client_product extends CI_Controller
         $this->load->view('feedback/error_alert');
         $this->load->view('navs/modals/exit_modal');
         $this->load->view('clients/show_product_details');
+    }
+
+
+    private function _initiate_pagination()
+    {
+        $this->pagination_library->set_uri_segment(4);
+        $this->count_products = $this->client_product_model->count_products();
+        $this->pagination_library->set_pagination_config($this->count_products);
+        $this->per_page = $this->pagination_library->get_per_page();
+        $this->page = $this->pagination_library->get_uri_segment();
     }
 }
