ace5b3b1a633cc7e055ce01552af4bf779398293
boniich <ezequieldbo25@gmail.com>
2023-12-15 19:25:01 -0300

    feat: Show data about admin in form to update this info

--- application/views/admin_panel/settings/admin/admin_settings_view.php
+++ application/views/admin_panel/settings/admin/admin_settings_view.php
@@ -18,24 +18,31 @@ defined('BASEPATH') or exit('No direct script access allowed');
             </div>
 
             <div class="w-50 bg-light shadow p-3 mb-5 bg-body rounded border border-secondary rounded-3">
-                <?php echo form_open('do_client_register'); ?>
-                <div class="col mb-3">
-                    <img src="../assets/amd.jpg" height="100" alt="..." class="rounded-circle" width="100">
-                    <input type="file" name="profile_image" accept="image/png, image/jpeg" />
-                </div>
-                <div class="col mb-3">
-                    <label for="full_name" class="form-label">Nombre completo</label>
-                    <input type="text" name="full_name" class="form-control" id="full_name" required>
-                </div>
-                <div class="col">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" id="email" required>
-                </div>
+                <?php foreach ($admin_data as $data) : ?>
+                    <?php echo form_open('do_client_register'); ?>
+                    <div class="col mb-3">
+                        <?php if ($data['image'] == NULL || $data['image'] == '') {
+                            $image = './assets/image-profile.png';
+                        } else {
+                            $image = './uploads/' . $data['image'];
+                        } ?>
+                        <img src="<?php echo base_url($image); ?>" class="rounded-circle" width="100" height="100"></img>
+                        <input type="file" name="profile_image" accept="image/png, image/jpeg" />
+                    </div>
+                    <div class="col mb-3">
+                        <label for="full_name" class="form-label">Nombre completo</label>
+                        <input type="text" name="full_name" class="form-control" id="full_name" value="<?php echo $data['full_name'] ?>" required>
+                    </div>
+                    <div class="col">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" value="<?php echo $data['email'] ?>" required>
+                    </div>
 
-                <div class="col mt-4">
-                    <button type="submit" name="submit" class="btn btn-success">Actualizar</button>
-                </div>
-                <?php echo form_close(); ?>
+                    <div class=" col mt-4">
+                        <button type="submit" name="submit" class="btn btn-success">Actualizar</button>
+                    </div>
+                    <?php echo form_close(); ?>
+                <?php endforeach; ?>
                 <?php if (isset($error_message)) : ?>
                     <div class="alert alert-danger p-2 m-0" role="alert">
                         <p class="text-center m-0 text-dark fs-5"><?php echo $error_message; ?></p>
