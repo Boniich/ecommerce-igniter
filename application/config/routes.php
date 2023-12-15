2992dc1f30ce27aaeb533e87c654ca3d91300ffe
boniich <ezequieldbo25@gmail.com>
2023-12-15 18:36:53 -0300

    feat: add new route to show the view for change the password of an admin

--- application/config/routes.php
+++ application/config/routes.php
@@ -87,6 +87,7 @@ $route['update_client'] = 'admin_panel/admin_panel_client/update_client/$1';
 $route['delete_client/(:num)'] = 'admin_panel/admin_panel_client/delete_client/$1';
 
 $route['admin_settings/profile'] = 'admin_panel/settings/admin_settings/index';
+$route['admin_settings/password'] = 'admin_panel/settings/admin_settings/index_password';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
