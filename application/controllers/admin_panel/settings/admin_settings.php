160e90244dd58c8fa7181696e14d8f0e7b37ee6d
boniich <ezequieldbo25@gmail.com>
2023-12-15 19:53:33 -0300

    feat: Add method to update the profile'data of an admin in the controller

--- application/controllers/admin_panel/settings/admin_settings.php
+++ application/controllers/admin_panel/settings/admin_settings.php
@@ -9,6 +9,7 @@ class Admin_settings extends CI_Controller
         $this->load->helper('form');
         $this->load->helper('url_helper');
         $this->load->library('sessions/sessions_library');
+        $this->load->library('session');
         $this->load->library('nav_library');
         $this->load->model('admin_panel/admin_data_model');
 
@@ -21,6 +22,7 @@ class Admin_settings extends CI_Controller
         $data['admin_data'] = $this->admin_data_model->get_admin($this->sessions_library->get_user_id());
         $this->load->view('head/head', $data);
         $this->nav_library->load_admin_nav();
+        $this->load->view('feedback/successfully_alert');
         $this->load->view('admin_panel/settings/admin/admin_settings_view');
     }
 
@@ -29,7 +31,27 @@ class Admin_settings extends CI_Controller
         $data['title'] = 'Ajustes - Password';
         $this->load->view('head/head', $data);
         $this->nav_library->load_admin_nav();
+        $this->load->view('feedback/successfully_alert');
         $this->load->view('admin_panel/settings/admin/admin_settings_password_view');
+    }
+
+    public function update_admin_profile()
+    {
+        $id = $this->sessions_library->get_user_id();
+        $full_name = $this->input->post('full_name');
+        $email = $this->input->post('email');
+
+        if (empty($_FILES['profile_image']['name'])) {
+            $data = array(
+                'full_name' => $full_name,
+                'email' => $email,
+            );
+        }
+
+        if ($this->admin_data_model->update_admin_profile($id, $data)) {
+            $this->session->set_flashdata('sucessfully_alert', 'Informacion de perfil actualizada');
+            redirect('admin_settings/profile');
+        }
     }
 
     private function _check_auth()
