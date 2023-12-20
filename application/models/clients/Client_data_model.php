9a5acd4f10851301622513a0e2469c3421b45a49
boniich <ezequieldbo25@gmail.com>
2023-12-19 21:46:17 -0300

    feat: add all necessary methods to update the profile's data

--- application/models/clients/Client_data_model.php
+++ application/models/clients/Client_data_model.php
@@ -26,4 +26,33 @@ class Client_data_model extends CI_Model
         $result = $query->result_array();
         return $result;
     }
+
+    public function check_email($id, $email)
+    {
+        $query = $this->db->where('id !=', $id)->where('email', $email)->get($this->_table);
+        $is_email_register = $query->num_rows() > 0 ?? true ?? false;
+        if ($is_email_register) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function check_dni($id, $dni)
+    {
+        $query = $this->db->where('id !=', $id)->where('dni', $dni)->get($this->_table);
+        $is_dni_taked = $query->num_rows() > 0 ?? true ?? false;
+        if ($is_dni_taked) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function update_client_profile($id, $data)
+    {
+        $this->db->where('id', $id);
+        $result = $this->db->update($this->_table, $data);
+        return $result;
+    }
 }
